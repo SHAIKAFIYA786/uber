328a8d0a95a86d64242f3520f8238807e74f20c8
Shaik Afiya <shaikafiya9676@gmail.com>
2025-05-09 14:29:15 +0530

    render yaml file updaated

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,11 +1,38 @@
+// const mongoose = require("mongoose");
+
+// function connectdb() {
+//   mongoose
+//     .connect(
+//       process.env.DB_CONNECT
+//       // It's good to include this option for stability
+//     )
+//     .then(() => {
+//       console.log("Connected to DB");
+//     })
+//     .catch((err) => {
+//       console.log("Error connecting to DB: ", err);
+//     });
+// }
+// module.exports = connectdb;
+
+
 const mongoose = require("mongoose");
 
 function connectdb() {
+  const dbURL =
+    process.env.NODE_ENV === "production"
+      ? process.env.PROD_DB_URI // production DB connection URL (MongoDB Atlas)
+      : process.env.LOCAL_DB_URI; // development DB connection URL (local MongoDB)
+
+  // Log the database you're connecting to
+  if (process.env.NODE_ENV === "production") {
+    console.log("Connecting to MongoDB Atlas...");
+  } else {
+    console.log("Connecting to local MongoDB...");
+  }
+
   mongoose
-    .connect(
-      process.env.DB_CONNECT
-      // It's good to include this option for stability
-    )
+    .connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
       console.log("Connected to DB");
     })
@@ -13,4 +40,6 @@ function connectdb() {
       console.log("Error connecting to DB: ", err);
     });
 }
+
 module.exports = connectdb;
+
